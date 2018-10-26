8ba17e821db6043f52bee65a5dfae303adaa03a3
hsjeevan <hsjeevan07@gmail.com>
2018-10-26 16:34:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var temperatureInCelsius=40;
+var celsiusToFahrenheit;
+celsiusToFahrenheit=(temperatureInCelsius * 9) / 5 + 32;
+var temperatureInFahrenheit=104
+var fahrenheitToCelsius;
+fahrenheitToCelsius=((temperatureInFahrenheit - 32) * 5) / 9;
+
+console.log('Temperatue of '+temperatureInCelsius+' Celcius, will be '+celsiusToFahrenheit+' Fahrenheit')
+console.log('Temperatue of '+temperatureInFahrenheit+' Fahrenheit, will be '+fahrenheitToCelsius+' Celcius')
